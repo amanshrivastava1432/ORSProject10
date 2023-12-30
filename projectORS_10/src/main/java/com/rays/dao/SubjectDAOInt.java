5d538dbd96e93a7deb16bdea7604375f4ba02952
amanshrivastava1432 <amanshrivastava1234@gmail.com>
2023-12-30 15:29:18 +0530

    Done

---
+++ projectORS_10/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Aman Shrivastava
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
